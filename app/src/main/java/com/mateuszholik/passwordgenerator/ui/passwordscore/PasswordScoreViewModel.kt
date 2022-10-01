70cdcc27878b9433fff539469369b67212397586
holmat98 <holmat1998@gmail.com>
2022-10-01 15:56:28 +0200

    displaying validation results in password score screen

--- app/src/main/java/com/mateuszholik/passwordgenerator/ui/passwordscore/PasswordScoreViewModel.kt
+++ app/src/main/java/com/mateuszholik/passwordgenerator/ui/passwordscore/PasswordScoreViewModel.kt
@@ -1,23 +1,33 @@
 package com.mateuszholik.passwordgenerator.ui.passwordscore
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.mateuszholik.data.repositories.models.Password
 import com.mateuszholik.domain.usecase.CalculatePasswordScoreUseCase
 import com.mateuszholik.passwordgenerator.R
 import com.mateuszholik.passwordgenerator.extensions.addTo
 import com.mateuszholik.passwordgenerator.extensions.subscribeWithObserveOnMainThread
 import com.mateuszholik.passwordgenerator.ui.base.BaseViewModel
+import com.mateuszholik.passwordvalidation.models.PasswordValidationResult
+import com.mateuszholik.passwordvalidation.usecases.ValidatePasswordUseCase
 import io.reactivex.rxjava3.schedulers.Schedulers
 import timber.log.Timber
 
 class PasswordScoreViewModel(
     private val password: String,
-    private val calculatePasswordScoreUseCase: CalculatePasswordScoreUseCase
+    private val calculatePasswordScoreUseCase: CalculatePasswordScoreUseCase,
+    private val validatePasswordUseCase: ValidatePasswordUseCase
 ) : BaseViewModel() {
 
     val passwordScore = MutableLiveData<Int>()
 
+    private val _validationResult = MutableLiveData<PasswordValidationResult>()
+    val validationResult: LiveData<PasswordValidationResult>
+        get() = _validationResult
+
     init {
         calculatePasswordScore()
+        validatePassword()
     }
 
     private fun calculatePasswordScore() {
@@ -31,5 +41,17 @@ class PasswordScoreViewModel(
                 }
             )
             .addTo(compositeDisposable)
+    }
+
+    private fun validatePassword() {
+        validatePasswordUseCase(password)
+            .subscribeWithObserveOnMainThread(
+                doOnNext = { _validationResult.value = it },
+                doOnSuccess = { Timber.i("Password validation has finished") },
+                doOnError = {
+                    Timber.e(it, "Error occurred during password validation")
+                    _errorOccurred.value = R.string.password_validation_result_error
+                }
+            )
     }
 }
