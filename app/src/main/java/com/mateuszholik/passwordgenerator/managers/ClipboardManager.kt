8f1672c9e60d890bdda3e0cc441d6c29859894b0
holmat98 <holmat1998@gmail.com>
2022-11-05 12:49:54 +0100

    Updated target_sdk to Android 13

--- app/src/main/java/com/mateuszholik/passwordgenerator/managers/ClipboardManager.kt
+++ app/src/main/java/com/mateuszholik/passwordgenerator/managers/ClipboardManager.kt
@@ -2,6 +2,8 @@ package com.mateuszholik.passwordgenerator.managers
 
 import android.content.ClipData
 import android.content.Context
+import android.os.Build
+import android.os.PersistableBundle
 import com.mateuszholik.passwordgenerator.R
 import com.mateuszholik.passwordgenerator.providers.MessageProvider
 
@@ -19,10 +21,16 @@ class ClipboardManagerImpl(
         val clipboard =
             context.getSystemService(Context.CLIPBOARD_SERVICE) as android.content.ClipboardManager
 
-        val clipData = ClipData.newPlainText(label, text)
+        val clipData = ClipData.newPlainText(label, text).apply {
+            description.extras = PersistableBundle().apply {
+                putBoolean("android.content.extra.IS_SENSITIVE", true)
+            }
+        }
 
         clipboard.setPrimaryClip(clipData)
 
-        messageProvider.show(R.string.message_password_copied_to_clipboard)
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S) {
+            messageProvider.show(R.string.message_password_copied_to_clipboard)
+        }
     }
 }
