6b2bc23ad989e9b401226c15e7f0ff95ad7b4e30
holmat98 <holmat1998@gmail.com>
2023-10-21 14:57:21 +0200

    57: added configure extensions method for LibraryExtension and BaseAppModuleExtension

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,52 +17,10 @@ androidGitVersion {
     multiplier = 100
 }
 
-android {
-    namespace = "com.mateuszholik.passwordgenerator"
-    compileSdk = DefaultConfig.COMPILE_SDK
-
-    buildFeatures {
-        buildConfig = true
-    }
-
-    defaultConfig {
-        applicationId = DefaultConfig.APPLICATION_ID
-        minSdk = DefaultConfig.MIN_SDK
-        targetSdk = DefaultConfig.TARGET_SDK
-        versionCode = androidGitVersion.code()
-        versionName = androidGitVersion.name()
-
-        testInstrumentationRunner = DefaultConfig.TEST_INSTRUMENTATION_RUNNER
-    }
-
-    buildFeatures {
-        viewBinding = true
-        dataBinding = true
-    }
-
-    buildTypes {
-        debug {
-            applicationIdSuffix = ".debug"
-        }
-        release {
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile(Proguard.FILE),
-                Proguard.RULES
-            )
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
+android.configure(
+    versionCode = androidGitVersion.code(),
+    versionName = androidGitVersion.name()
+)
 
 dependencies {
 
--- passwordvalidation/build.gradle.kts
+++ passwordvalidation/build.gradle.kts
@@ -5,36 +5,9 @@ plugins {
     id(Plugins.JACOCO)
 }
 
-android {
-    namespace = "com.mateuszholik.passwordvalidation"
-    compileSdk = DefaultConfig.COMPILE_SDK
-
-    defaultConfig {
-        minSdk = DefaultConfig.MIN_SDK
-
-        testInstrumentationRunner = DefaultConfig.TEST_INSTRUMENTATION_RUNNER
-    }
-
-    buildTypes {
-        release {
-            proguardFiles(
-                getDefaultProguardFile(Proguard.FILE),
-                Proguard.RULES
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
+android.configure(namespace = "com.mateuszholik.passwordvalidation")
 
 dependencies {
-
-    // Dependencies
     common()
     unitTesting()
     room()
